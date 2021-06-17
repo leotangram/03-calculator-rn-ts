8f92a14da2f872f455e54b91d61af08006fa1265
Leonardo Omaña <leotangram@gmail.com>
2021-06-17 11:47:10 -0500

    Text & first button

--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -3,10 +3,35 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   background: {
     backgroundColor: '#000000',
-    flex: 1
+    flex: 1,
   },
-  text: {
+  calculatorContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 10
+  },
+  result: {
     color: '#ffffff',
-    fontSize: 20
+    fontSize: 60,
+    textAlign: 'right'
+  },
+  smallResult: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    fontSize: 30,
+    textAlign: 'right'
+  },
+  button: {
+    backgroundColor: '#9b9b9b',
+    borderRadius: 100,
+    height: 80,
+    justifyContent: 'center',
+    width: 80
+  },
+  textButton: {
+    color: '#000000',
+    fontSize: 30,
+    fontWeight: '300',
+    padding: 10,
+    textAlign: 'center'
   }
 });
