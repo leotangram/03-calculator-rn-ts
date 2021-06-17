29236427e8685f1642360fef0a925e63002ef550
Leonardo Omaña <leotangram@gmail.com>
2021-06-17 11:31:05 -0500

    Init design & calculator screen

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import { styles } from '../theme/appTheme';
+
+const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Calculator Screen</Text>
+    </View>
+  )
+}
+
+export default CalculatorScreen
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#000000',
+    flex: 1
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 20
+  }
+});
