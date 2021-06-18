895e7ef957c8845808817627a93bb0f242438a21
Leonardo Omaña <leotangram@gmail.com>
2021-06-17 19:27:48 -0500

    Validations to build the number

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -5,22 +5,48 @@ import ButtonCalculator from '../components/ButtonCalculator'
 
 const gray = '#9b9b9b'
 const orange = '#ff9427'
+const negative = '-'
+const dot = '.'
+const zero = '0'
 
 const CalculatorScreen = () => {
-  const [number, setNumber] = useState('0')
-  const [prevNumber, setPrevNumber] = useState('0')
+  const [number, setNumber] = useState(zero)
+  const [prevNumber, setPrevNumber] = useState(zero)
 
-  const clean = () => setNumber('0')
+  const clean = () => setNumber(zero)
 
-  const buildNumber = (textNumber: string) => setNumber(number + textNumber)
+  const buildNumber = (textNumber: string) => {
+    if (number.includes(dot) && textNumber === dot) return
+    if (number.startsWith(zero) || number.startsWith(`${negative}${zero}`)) {
+      if (textNumber === dot) setNumber(number + textNumber)
+      else if (textNumber === zero && number.includes(dot))
+        setNumber(number + textNumber)
+      else if (textNumber !== zero && !number.includes(dot))
+        setNumber(textNumber)
+      else if (textNumber === zero && !number.includes(dot)) setNumber(number)
+      else setNumber(number + textNumber)
+    } else {
+      setNumber(number + textNumber)
+    }
+  }
+
+  const positiveNegative = () => {
+    if (number.includes(negative)) {
+      setNumber(number.replace(negative, ''))
+    } else {
+      setNumber(negative + number)
+    }
+  }
 
   return (
     <View style={styles.calculatorContainer}>
       <Text style={styles.smallResult}>{prevNumber}</Text>
-      <Text adjustsFontSizeToFit numberOfLines={1} style={styles.result}>{number}</Text>
+      <Text adjustsFontSizeToFit numberOfLines={1} style={styles.result}>
+        {number}
+      </Text>
       <View style={styles.row}>
         <ButtonCalculator color={gray} onPress={clean} text="C" />
-        <ButtonCalculator color={gray} onPress={clean} text="+/-" />
+        <ButtonCalculator color={gray} onPress={positiveNegative} text="+/-" />
         <ButtonCalculator color={gray} onPress={clean} text="del" />
         <ButtonCalculator color={orange} onPress={clean} text="/" />
       </View>
@@ -34,7 +60,7 @@ const CalculatorScreen = () => {
         <ButtonCalculator onPress={buildNumber} text="4" />
         <ButtonCalculator onPress={buildNumber} text="5" />
         <ButtonCalculator onPress={buildNumber} text="6" />
-        <ButtonCalculator color={orange} onPress={clean} text="-" />
+        <ButtonCalculator color={orange} onPress={clean} text={negative} />
       </View>
       <View style={styles.row}>
         <ButtonCalculator onPress={buildNumber} text="1" />
@@ -43,8 +69,8 @@ const CalculatorScreen = () => {
         <ButtonCalculator color={orange} onPress={clean} text="+" />
       </View>
       <View style={styles.row}>
-        <ButtonCalculator onPress={buildNumber} text="0" width />
-        <ButtonCalculator onPress={buildNumber} text="." />
+        <ButtonCalculator onPress={buildNumber} text={zero} width />
+        <ButtonCalculator onPress={buildNumber} text={dot} />
         <ButtonCalculator color={orange} onPress={clean} text="=" />
       </View>
     </View>
